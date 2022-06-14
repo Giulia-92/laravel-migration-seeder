ffb4815b772b7c04acbd47e1e2efa56651c52131
Giulia-92 <CIONCO.GIULIA@GMAIL.COM>
2022-06-14 17:57:20 +0200

    first commit

---
+++ database/migrations/2022_06_14_145607_create_travels_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travels', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',120);
+            $table->string('address');
+            $table->float('day');
+            $table->date('dateinizio');
+            $table->date('datefine');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travels');
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.style')
+@section('title', 'Homepage')
+    @section('main')
+<h1 class="text-center">Home</h1>   
+@endsection
+
+
+
